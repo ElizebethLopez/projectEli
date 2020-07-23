83e42a798f6c585dd03fae14ecc13d4ef1582212
jai rino lumi <jairino@gmail.com>
2020-07-22 23:31:30 -0400

    making myfirst commit src folder

---
+++ src/preli/myClass.java
@@ -0,0 +1,18 @@
+package preli;
+
+public class myClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world, this is my first version");
+	
+		
+		for(int i=0; i<10;i++)
+		{
+			System.out.println(i);
+		}
+			
+			
+
+	}
+
+}
